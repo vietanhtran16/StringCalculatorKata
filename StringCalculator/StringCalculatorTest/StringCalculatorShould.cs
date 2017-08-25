388a81cd642fc0bb5aff220db9ba0a77be9fe4e1
Viet Anh Tran MYOB <viet.anhtran@myob.com>
2017-08-25 15:10:43 +1200

    Fixed Project Move

---
+++ StringCalculator/StringCalculatorTest/StringCalculatorShould.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace StringCalculatorTest
+{
+    public class StringCalculatorShould
+    {
+        [Fact]
+        public void Add_WhenStringIsEmpty_ReturnZero()
+        {
+            var stringCalculator = new StringCalculator();
+            Assert.Equal(0, stringCalculator.Add(""));
+        }
+    }
+}
