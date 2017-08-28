9504f127fbf115308a103e5a5441be49b61f07b4
Viet Anh Tran MYOB <viet.anhtran@myob.com>
2017-08-28 12:50:30 +1200

    Unit test add for three multiple correct numbers (passed).

--- StringCalculator/StringCalculatorTest/StringCalculatorShould.cs
+++ StringCalculator/StringCalculatorTest/StringCalculatorShould.cs
@@ -9,6 +9,7 @@ namespace StringCalculatorTest
         [InlineData(0, "")]
         [InlineData(1, "1")]
         [InlineData(3, "1,2")]
+        [InlineData(6, "1,2,3")]
         public void Add_ValidInputs_ReturnSum(int expected, string numbers)
         {
             var stringCalculator = new StringCalculator();
